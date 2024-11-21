09806e6c7e1f503232fc18ea8f0813f5a1e960fc
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-11-21 07:54:05 +0300

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender);
-  let img = 'https://i.imgur.com/s0SqG3g.jpeg';
+  let img = 'https://i.ibb.co/VQ05SWb/f188fefb6f97a756.jpg';
   let con = {
     key: {
       fromMe: false,
@@ -17,14 +17,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   };
 
   let messageContent = {
-    text: '𝗨𝗟𝗧𝗥𝗔-𝗠𝗗 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
+    text: '*CODEZAP* 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '© 𝙶𝚕𝚘𝚋𝚊𝚕𝚃𝚎𝚌𝚑𝙸𝚗𝚏𝚘',
-        body: '© GlobalTechInfo',
+        title: '© Mbuvi Tech',
+        body: '© Mbuvi Tech',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
+        sourceUrl: 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
