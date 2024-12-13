32fa62180af7c76f6f3153b4311c7e78d523e31e
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-12-13 18:36:14 +0300

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import pkg from 'nayan-media-downloader';
+import pkg from 'nayan-videos-downloader';
 const { ytdown } = pkg;
 
 const fetchWithRetry = async (url, options, retries = 3) => {
@@ -45,7 +45,7 @@ const handler = async (m, { args, conn, usedprefix }) => {
         }
 
         const title = response.data.title || 'video';
-        const caption = `𝘗𝘖𝘞𝘌𝘙𝘌𝘋 𝘉𝘠 © 𝘜𝘓𝘛𝘙𝘈-𝘔𝘋`;
+        const caption = `POWERED BY © CODEZAP`;
 
         // Fetch the video file with retry
         const mediaResponse = await fetchWithRetry(videoUrl, {
